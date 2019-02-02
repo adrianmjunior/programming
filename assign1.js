f5ce481927cf08d9174a54d29bff7d6bf81b239e
Adrian Martinez <adrjr01@hotmail.com>
2019-02-02 02:14:23 -0600

    Finshed assignment 2
    Added an array, as well as as some conditional statements and tried to incorporate it into the CSS

--- assign1.js
+++ assign1.js
@@ -1,11 +1,33 @@
-// var date = new Date();
-//     document.getElementById("date").innerHTML = "<h1>Today is: " + date + "</h1>";
+
 
 var intro = "strings are typically words.";
-    document.getElementsByClassName("date").innerHTML = "<h1> Did you know " + intro + "</h1>";
+    document.getElementById("date").innerHTML = "<h1> Did you know " + intro + "</h1>";
 
-var a = 10;
+var a = 2;
 var b = 25;
 var sum = a + b;
 
-document.getElementById("vari").innerHTML = "<p> The value of variable a is " + a + ".</p>";
+document.getElementById("vari").innerHTML = "<h2> The value of variable \"a\" is " + a + ". The value of variable \"b\" is " + b + ". Their \"sum\" is " + sum + ".</h2>";
+
+var random;
+random = ["dog", "cat", "pig", "cat", "rat"];
+
+
+random[1] == random [3] ? console.log("Cats will be cats :)") : console.log();
+
+
+
+
+a == b ? alert("YAY! \"a\" and \"b\" are equal!") : alert("BOO! \"a\" and \"b\" are not the same");
+
+
+
+
+// Longhand for statement above
+// if (a == b) {
+//     console.log("YAY! \"a\" and \"b\" are equal!"");
+// } else {
+//     console.log("BOO! \"a\" and \"b\" are not the same"");
+// }
+
+

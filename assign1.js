38bd4202ca338325b262bc13f2aa035d855c810f
Adrian Martinez <adrjr01@hotmail.com>
2019-02-02 00:50:02 -0600

    JavaScript practice
    I added some string data to document and am trying to learn how to intergate it with HTML

--- assign1.js
+++ assign1.js
@@ -1,2 +1,11 @@
-var date = new Date();
-    document.getElementById("example").innerHTML = "<h1>Today is: " + date + "</h1>";
+// var date = new Date();
+//     document.getElementById("date").innerHTML = "<h1>Today is: " + date + "</h1>";
+
+var intro = "strings are typically words.";
+    document.getElementsByClassName("date").innerHTML = "<h1> Did you know " + intro + "</h1>";
+
+var a = 10;
+var b = 25;
+var sum = a + b;
+
+document.getElementById("vari").innerHTML = "<p> The value of variable a is " + a + ".</p>";
